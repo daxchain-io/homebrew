30b695e8c5b89f1c39e00ab8b4dc3285d6e061dc
Scott Keith Thomas, Jr <skthomasjr@users.noreply.github.com>
2023-05-16 08:17:09 -0600

    Update cli-wallet.rb

--- cli-wallet.rb
+++ cli-wallet.rb
@@ -1,4 +1,4 @@
-class CLI-Wallet < Formula
+class CLIWallet < Formula
   desc "A command-line interface based Ethereum wallet with token and smart contract support"
   homepage "https://github.com/daxchain-io/cli-wallet"
   url "https://github.com/daxchain-io/cli-wallet/archive/refs/tags/v0.0.0.tar.gz
