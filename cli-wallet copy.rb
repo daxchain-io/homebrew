1541c91b201df9d59163c266a6cb493e2e0a45c3
Scott Thomas <scott@Scotts-MacBook-Pro-M1.local>
2023-05-16 22:12:17 -0600

    use brew bottle

--- cli-wallet copy.rb
+++ cli-wallet copy.rb
@@ -1,7 +1,7 @@
 class CliWallet < Formula
   desc "A command-line interface based Ethereum wallet with token and smart contract support"
   homepage "https://github.com/daxchain-io/cli-wallet"
-  url "https://github.com/daxchain-io/cli-wallet/archive/refs/tags/v0.0.0.tar.gz"
+  url "https://github.com/daxchain-io/cli-wallet/archive/refs/tags/v0.0.5.tar.gz"
   sha256 "0178a246efe4d7c5beda8c2dbedde1dab41a12de7a64d4d589c1683cc9023b99"
   
   depends_on "go" => :build
