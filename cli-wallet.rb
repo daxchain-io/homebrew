1fb63b5b6db40a83b5e384bece7820547b84aef2
GitHub Action <actions@github.com>
2023-05-17 04:32:43 +0000

    Update cli-wallet to

--- cli-wallet.rb
+++ cli-wallet.rb
@@ -1,8 +1,8 @@
 class CliWallet < Formula
   desc "A command-line interface based Ethereum wallet with token and smart contract support"
   homepage "https://github.com/daxchain-io/cli-wallet"
-  url "https://github.com/daxchain-io/cli-wallet/archive/refs/tags/v0.0.5.tar.gz"
-  sha256 "4ee96d8cc807efcbc14d747e2c761c16fd55949b147b90f3829b626ddc77a59e"
+  url "https://github.com/daxchain-io/cli-wallet/archive/refs/tags/v0.0.6.tar.gz"
+  sha256 "703c62492cc77a15eeadeecfd273264e195f92c15942756b3300fc8c91b562f1"
   
   depends_on "go" => :build
 
